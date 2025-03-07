4992ee9c1e7351cfec71a02fd53f2da3cabcf482
2juju <167789341+2juju@users.noreply.github.com>
2025-03-07 21:05:04 +0000

    msg

--- src/main/StockSystem.java
+++ src/main/StockSystem.java
@@ -1,2 +1,40 @@
 package main;
-i
+import java.util.Scanner;
+import managers.StockManager;
+import models.Product;
+
+public class StockSystem {
+    public static void main(String[] args) {
+
+        StockManager stockManager = new StockManager();
+        // test items
+        Scanner inputObj  = new Scanner(System.in);
+
+        System.out.println("Enter the product id: ");
+        String id = inputObj.nextLine();
+
+        System.out.println("Enter the product name: ");
+        String name = inputObj.nextLine();
+
+        System.out.println("Enter the product quantity: ");
+        int quantity = inputObj.nextInt();
+
+
+        System.out.println("Enter the product price: ");
+        double price = inputObj.nextDouble();
+
+
+        Product product = new Product(id, name, quantity, price);
+        stockManager.addProduct(product);
+
+        System.out.println("Stock list: ");
+        stockManager.listProducts();
+
+
+        // Product product = stockManager.searchProductById("");
+        // System.out.println(product);
+        // stockManager.removeProduct(product);
+        // stockManager.listProducts();
+        inputObj.close();
+    }
+}
