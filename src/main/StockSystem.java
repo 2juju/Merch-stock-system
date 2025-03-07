cca688dd843964e3369cce163438435d2989a6e5
2juju <167789341+2juju@users.noreply.github.com>
2025-03-07 19:24:00 +0000

    created stockmanager(inventory) and stocksystem(main runner) classes

--- src/main/StockSystem.java
+++ src/main/StockSystem.java
@@ -1,2 +1,24 @@
 package main;
+import managers.StockManager;
+import models.Product;
 
+public class StockSystem {
+    public static void main(String[] args) {
+
+        StockManager stockManager = new StockManager();
+        // test items
+        Product product1 = new Product("1", "Apple", 10, 0.5);
+        Product product2 = new Product("2", "Banana", 20, 0.3);
+        Product product3 = new Product("3", "Orange", 15, 0.4);
+
+        stockManager.addProduct(product1);
+        stockManager.addProduct(product2);
+        stockManager.addProduct(product3);
+        stockManager.listProducts();
+
+        Product product = stockManager.searchProductById("2");
+        System.out.println(product);
+        stockManager.removeProduct(product);
+        stockManager.listProducts();
+    }
+}
--- src/managers/StockManager.java
+++ src/managers/StockManager.java
@@ -1,5 +1,56 @@
 package managers;
+// inventory management system, contains stock details, manages stock
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import models.Product;
 
 public class StockManager {
+    /**
+     * Stores products in the inventory, manages stock, adds products to stock
+     * @param products - list of products
+     */
+    private ArrayList<Product> products = new ArrayList<>();
+
+    /**
+     * Adds a product to the inventory
+     * @param product
+     */
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println(product.getName() + " added to stock");
+    }
+
+    public void removeProduct(Product product){
+        products.remove(product);
+        System.out.println(product.getName() + " removed from stock");
+    } // ill change this later, make a helper function instead
+
+    // helper function
+    /**
+     * Searches for a product in the inventory using its ID or name
+     * @param predicate
+     * @return the product if found, null otherwise
+     */
+    public Product searchProduct(Predicate<Product> predicate){
+        for(Product product : products){
+            if(predicate.test(product)){
+                return product;
+            }
+        }
+        System.out.println("Product not found");
+        return null;
+    }
+    public Product searchProductById(String id){
+        return searchProduct(product -> product.getId().equals(id));
+    }
+    public Product searchProductByName(String name){
+        return searchProduct(product -> product.getName().equals(name));
+    }
+
+    public void listProducts(){
+        for(Product product : products){
+            System.out.println(product);
+        }
+    }
     
 }
