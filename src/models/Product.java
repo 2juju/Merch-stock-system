ae0f369e09e49c3c11d81a95133199ecb92c57ef
2juju <167789341+2juju@users.noreply.github.com>
2025-03-07 15:32:49 +0000

    uploaded files to the repository

---
+++ src/models/Product.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Product {
+    private String id;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product(String id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
