3443d0e97f46d4917d4993189e294ce843b8aca2
2juju <167789341+2juju@users.noreply.github.com>
2025-03-07 17:13:03 +0000

    added javadocs

--- src/models/Product.java
+++ src/models/Product.java
@@ -1,28 +1,51 @@
 package models;
+// represents each product (item) in the stock system, contains product details,
+/**
+* Product class to store product details
+* represents each product (item) in the stock system
+* author 2juju
+* version 1.0
+*/
 
 public class Product {
+
     private final String id;
     private final String name;
     private int quantity;
     private double price;
-    // variable to store product details
 
+    /**
+     * Constructor to initialise product details
+     * @param id - unique identifier for product
+     * @param name - name of product
+     * @param quantity - quantity of product
+     * @param price - price of product
+     * @return - product details
+     */
     public Product(String id, String name, int quantity, double price) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
         this.price = price;
-        // constructor to initialize product details
     }
+
+    /**
+    * Updates the quantity of the product
+    * @param quantity - quantity of product
+    */
     public void updateQuantity(int quantity) {
+
         this.quantity += quantity;
         System.out.println(name + " quantity updated to " + this.quantity);
-        // method to update quantity of product
     }
+
+    /**
+     * Updates the price of the product
+     * @param price - price of product
+     */
     public void updatePrice(double price) {
         this.price = price;
         System.out.println(name + " price updated to " + this.price);
-        // method to update price of product
         // may delete later - this is just for testing
     }
     public String getId(){return id;}
@@ -40,6 +63,5 @@ public class Product {
                 '}';
         // method to return product details
     }
-
 }
 
